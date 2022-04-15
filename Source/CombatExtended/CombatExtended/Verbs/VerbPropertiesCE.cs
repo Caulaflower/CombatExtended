4489d0ac4d86a289038315ecf9d4327c040998f2
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2022-04-15 22:13:00 +0200

    added barrel length
    added barrel length

--- Source/CombatExtended/CombatExtended/Verbs/VerbPropertiesCE.cs
+++ Source/CombatExtended/CombatExtended/Verbs/VerbPropertiesCE.cs
@@ -18,6 +18,9 @@ namespace CombatExtended
         public float meleeArmorPenetration = 0;
         public bool ejectsCasings = true;
         public bool ignorePartialLoSBlocker = false;
+        public float speedMult = 1f;
+        // not a true floatrange, just takes the min for barrel length velocity mult and max for what distance it translates to
+        public FloatRange barrelLength;
 
         public float AdjustedArmorPenetrationCE(Verb ownerVerb, Pawn attacker)
         {
